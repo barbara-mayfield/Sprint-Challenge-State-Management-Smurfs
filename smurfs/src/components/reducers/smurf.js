0a968d06ba52116ed86b39ca52e7be8492e5aceb
Barbara Mayfield <bmayfield1991@gmail.com>
2019-10-25 19:10:27 -0500

    Files scaffolded, created store in index.js and returning default state in reducer

---
+++ smurfs/src/components/reducers/smurf.js
@@ -0,0 +1,13 @@
+export const initialState = {
+    "name":"Brainey",
+    "age":200,
+    "height":"5cm",
+    "id":0    
+}
+
+export function reducer(state = initialState, action) {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+}
